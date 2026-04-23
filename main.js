5e7bb049504ef9761fa33ce9bee502f225566395
Moto1206 <nthd20040612@gmail.com>
2026-04-23 16:57:14 +0900

    my frist commit -0-

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+// const answer = prompt("what is your name?");
+// alert("welcome to my web page");
+
+const target = document.getElementById("target");
+target.textContent = "あほほほほほ ";
+target.style.color = "red";
+target.style.fontSize = "30px";
+
+target.style.textAlign = "center";
+
