2ac6451bc49785b3b92a464c9305f5ef70dd9012
Moto1206 <nthd20040612@gmail.com>
2026-04-23 17:57:53 +0900

    counter fun create

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
+function sayHello() {
+    alert("CANCEL!");
+}
 
-
-// const answer = prompt("what is your name?");
-// alert("welcome to my web page");
-
-const target = document.getElementById("target");
-target.textContent = "あほほほほほ ";
-target.style.color = "red";
-target.style.fontSize = "30px";
-
-target.style.textAlign = "center";
 
